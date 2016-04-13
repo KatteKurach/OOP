acc99706b0e995b816e0084f542eb1abd869bd07
KatteKurach <kattekurach@gmail.com>
2016-04-13 10:29:56 +0300

    Second laba is finished.

---
+++ laba2.java
@@ -0,0 +1,102 @@
+import java.io.*
+import java.lang.*
+import java.util.*
+
+class laba2
+{
+    public static void main(String[] args) {}
+}
+
+
+class Cat_Tom
+{
+    public Cat_Tom() {}
+
+    public void run() {}
+    public void jump() {}
+    public void take() {}
+    public boolean see_vacuum_machine() {}
+}
+
+class Mouse_Jerry
+{
+    public Mouse_Jerry() {}
+
+    public void take_cheese() {}
+    public void run(){}
+    public boolean turn_on(){}
+}
+
+class Cheese
+{
+    public Cheese() {}
+
+    public void get_bonus_ten() {}
+    public void give_health() {}
+}
+
+class Flower
+{
+    public Flower() {}
+
+    public boolean hide_mouse(){}
+}
+
+class Papper
+{
+    public Papper(){}
+
+    public boolean mouse_acceleration(){}
+    public boolean cat_slowdown(){}
+}
+
+class Cherry
+{
+    public Cherry(){}
+
+    public boolean mouse_slow(){}
+    public boolean cat_slow(){}
+}
+
+class Clock
+{
+    public Clock() {}
+
+    public void cat_stop(){}
+    public boolean block_hole(){}
+    public boolean block_papper(){}
+}
+
+
+class Black_oil
+{
+    public Black_oil(){}
+
+    public void cat_slip(){}
+    public void mouse_slip(){}
+}
+
+
+class Hole
+{
+    public Hole(){}
+
+    public boolean open(){}
+    public boolean cat_close(){}
+}
+
+class Vacuum_cleaner
+{
+    public Vacuum_cleaner() {}
+
+    public boolean cat_run_away(){}
+    public boolean mouse_turn_on(){}
+}
+
+class Apple
+{
+    public Apple(){}
+
+    public int mouse_bonus(){}
+}
+
